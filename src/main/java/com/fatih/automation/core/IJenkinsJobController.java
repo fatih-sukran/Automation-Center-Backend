ddc35a7b026e8bcb286024ed9624858cd74984f8
Fatih Şükran <fth.21.81@gmail.com>
2023-11-22 21:15:17 +0300

    update JenkinsJobController

--- src/main/java/com/fatih/automation/core/IJenkinsJobController.java
+++ src/main/java/com/fatih/automation/core/IJenkinsJobController.java
@@ -1,8 +1,12 @@
 package com.fatih.automation.core;
 
+import com.fatih.automation.core.data.TestStatus;
+import com.fatih.automation.core.model.TestBuild;
+import com.fatih.automation.core.model.TestMethod;
+
 public interface IJenkinsJobController {
 
-    void runTest();
-    String getStatus();
-    String getTestResult();
+    TestBuild runTest(long id);
+    TestBuild runTest(TestMethod testMethod);
+    TestStatus getStatus(TestBuild testBuild);
 }
--- src/main/java/com/fatih/automation/core/JenkinsJobController.java
+++ src/main/java/com/fatih/automation/core/JenkinsJobController.java
@@ -1,33 +1,46 @@
 package com.fatih.automation.core;
 
+import com.fatih.automation.core.data.TestStatus;
+import com.fatih.automation.core.model.TestBuild;
+import com.fatih.automation.core.model.TestMethod;
 import com.offbytwo.jenkins.JenkinsServer;
 import lombok.SneakyThrows;
+import org.springframework.stereotype.Service;
 
 import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
+@Service
 public class JenkinsJobController implements IJenkinsJobController {
 
-    private final JenkinsServer JENKINS_SERVER;
-    private final String JOB_NAME;
+    private static final String JOB_NAME = "Automation";
+    private static final String JENKINS_URL = "http://localhost:8081";
+    private static final String JENKINS_USER = "admin";
+    private static final String JENKINS_PASSWORD = "admin";
+    private static final Map<UUID, TestBuild> JOB_BUILDS = new HashMap<>();
+    private final JenkinsServer JENKINS_SERVER = new JenkinsServer(new URI(JENKINS_URL), JENKINS_USER, JENKINS_PASSWORD);
+
+
+    public JenkinsJobController() throws URISyntaxException {}
 
     @SneakyThrows
-    public JenkinsJobController(String jobName) {
-        JENKINS_SERVER = new JenkinsServer(new URI("http://localhost:8081"), "admin", "admin");
-        JOB_NAME = jobName;
-    }
-
     @Override
-    public void runTest() {
-
-    }
-
-    @Override
-    public String getStatus() {
+    public TestBuild runTest(long id) {
+        JENKINS_SERVER.getJob(JOB_NAME).build(true);
         return null;
     }
 
     @Override
-    public String getTestResult() {
+    public TestBuild runTest(TestMethod testMethod) {
+        JENKINS_SERVER.getJob(JOB_NAME).build(true);
+        return null;
+    }
+
+    @Override
+    public TestStatus getStatus(TestBuild testBuild) {
         return null;
     }
 }
