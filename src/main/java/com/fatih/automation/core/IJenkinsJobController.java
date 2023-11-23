f612a9f23d278a408dc388cb0f1626fd5fb97569
Fatih Şükran <fth.21.81@gmail.com>
2023-11-23 12:13:03 +0300

    update interface

--- src/main/java/com/fatih/automation/core/IJenkinsJobController.java
+++ src/main/java/com/fatih/automation/core/IJenkinsJobController.java
@@ -4,9 +4,11 @@ import com.fatih.automation.core.data.TestStatus;
 import com.fatih.automation.core.model.TestBuild;
 import com.fatih.automation.core.model.TestMethod;
 
+import java.util.UUID;
+
 public interface IJenkinsJobController {
 
-    TestBuild runTest(long id);
+    void runTest(long id);
     TestBuild runTest(TestMethod testMethod);
-    TestStatus getStatus(TestBuild testBuild);
+    TestStatus getStatus(UUID uuid);
 }
