3ab7fb9a8535f8187e74751af60c5635ad24afab
Fatih Şükran <fth.21.81@gmail.com>
2024-01-03 16:16:17 +0300

    Test Method Controller Tests

--- src/test/java/com/fatih/automation/test/controller/TestMethodControllerTests.java
+++ src/test/java/com/fatih/automation/test/controller/TestMethodControllerTests.java
@@ -1,13 +1,12 @@
 package com.fatih.automation.test.controller;
 
-import com.fatih.automation.Main;
+import com.fatih.automation.core.model.TestMethod;
+import com.jayway.jsonpath.JsonPath;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
-
-import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -18,9 +17,30 @@ public class TestMethodControllerTests {
     TestRestTemplate testRestTemplate;
 
     @Test
+    public void shouldReturnTestMethodWhenAddedValid() {
+        var testMethod = new TestMethod(null, "name", "description");
+        var testMethodResponse = testRestTemplate.postForEntity("/testmethods", testMethod, String.class);
+        assertThat(testMethodResponse.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+
+        var documentContext = JsonPath.parse(testMethodResponse.getBody());
+        String id = documentContext.read("$.id");
+        assertThat(id).isNotNull();
+
+        var name = documentContext.read("$.name");
+        assertThat(name).isEqualTo(testMethod.name());
+
+        var description = documentContext.read("$.description");
+        assertThat(description).isEqualTo(testMethod.description());
+    }
+
+    @Test
     public void shouldGetAllTestMethods() {
         var testMethodsResponse = testRestTemplate.getForEntity("/testmethods", String.class);
         assertThat(testMethodsResponse.getStatusCode()).isEqualTo(HttpStatus.OK);
         assertThat(testMethodsResponse.getBody()).isNotNull();
+
+        var documentContext = JsonPath.parse(testMethodsResponse.getBody());
+        int length = documentContext.read("$.length()");
+        assertThat(length).isEqualTo(0);
     }
 }
