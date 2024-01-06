5576c616c14298144010ac786b8f0284c456821a
Fatih Şükran <fth.21.81@gmail.com>
2024-01-06 16:20:37 +0300

    update AutomationApplication.java

--- src/main/java/com/fatih/automation/AutomationApplication.java
+++ src/main/java/com/fatih/automation/AutomationApplication.java
@@ -6,6 +6,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
 
 import java.io.File;
 
@@ -15,18 +18,21 @@ public class AutomationApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AutomationApplication.class, args);
 	}
-
-
 	@Bean
-	CommandLineRunner init(TestMethodRepository repository) {
-		return args -> {
-			var PATH = "/Users/fatih.sukran/Downloads/api/src/test";
-			var file = new File(PATH);
-			var methods = Main.printPaths(file);
-
-			methods.forEach(method -> {
-				repository.save(new TestMethod(method.getName().asString()));
-			});
-		};
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		return http.csrf(AbstractHttpConfigurer::disable).build();
 	}
+
+//	@Bean
+//	CommandLineRunner init(TestMethodRepository repository) {
+//		return args -> {
+//			var PATH = "/Users/fatih.sukran/Downloads/api/src/test";
+//			var file = new File(PATH);
+//			var methods = Main.printPaths(file);
+//
+//			methods.forEach(method -> {
+//				repository.save(new TestMethod(method.getName().asString()));
+//			});
+//		};
+//	}
 }
