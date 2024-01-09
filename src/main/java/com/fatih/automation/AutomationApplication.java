9053e7c8e6b1785d1c4499e9d152f2501c3b23d7
Fatih Şükran <fth.21.81@gmail.com>
2024-01-09 18:32:46 +0300

    update bean for when app started, read the tests

--- src/main/java/com/fatih/automation/AutomationApplication.java
+++ src/main/java/com/fatih/automation/AutomationApplication.java
@@ -1,10 +1,12 @@
 package com.fatih.automation;
 
-import com.fatih.automation.controller.TestMethod;
-import com.fatih.automation.controller.TestMethodRepository;
+import com.fatih.automation.model.TestMethod;
+import com.fatih.automation.repositories.TestMethodRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -12,27 +14,25 @@ import org.springframework.security.web.SecurityFilterChain;
 
 import java.io.File;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class AutomationApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AutomationApplication.class, args);
 	}
-	@Bean
-	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-		return http.csrf(AbstractHttpConfigurer::disable).build();
-	}
 
-//	@Bean
-//	CommandLineRunner init(TestMethodRepository repository) {
-//		return args -> {
-//			var PATH = "/Users/fatih.sukran/Downloads/api/src/test";
-//			var file = new File(PATH);
-//			var methods = Main.printPaths(file);
-//
-//			methods.forEach(method -> {
-//				repository.save(new TestMethod(method.getName().asString()));
-//			});
-//		};
-//	}
+	@Bean
+	CommandLineRunner init(TestMethodRepository repository) {
+		return args -> {
+			var PATH = "/Users/fatih.sukran/Downloads/api/src/test";
+			var file = new File(PATH);
+			var methods = Main.printPaths(file);
+
+			methods.forEach(testMethod -> {
+				if (!repository.existsByNameAndClassName(testMethod.getName(), testMethod.getClassName())) {
+					repository.save(testMethod);
+				}
+			});
+		};
+	}
 }
