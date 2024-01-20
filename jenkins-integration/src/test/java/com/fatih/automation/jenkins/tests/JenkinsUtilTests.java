241552770485b8c867b3ab4aee1c6ea36ba806b1
Fatih Şükran <fth.21.81@gmail.com>
2024-01-20 08:49:40 +0300

    * Created test for Jenkins Util class

---
+++ jenkins-integration/src/test/java/com/fatih/automation/jenkins/tests/JenkinsUtilTests.java
@@ -0,0 +1,55 @@
+package com.fatih.automation.jenkins.tests;
+
+import com.fatih.automation.jenkins.model.JenkinsJob;
+import com.fatih.automation.jenkins.model.JenkinsView;
+import com.fatih.automation.jenkins.utils.JenkinsUtil;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
+public class JenkinsUtilTests {
+
+    @Test
+    public void checkGetView() throws IOException {
+        var jenkinsUtil = new JenkinsUtil();
+        var view = jenkinsUtil.getView("all");
+
+        System.out.println("View: ");
+        System.out.println(view);
+
+        assertThat(view).isNotNull();
+        assertThat(view.getName()).isEqualTo("all");
+
+        jenkinsUtil.close();
+    }
+
+    @Test
+    public void checkGetAllViews() throws IOException {
+        var jenkinsUtil = new JenkinsUtil();
+        var views = jenkinsUtil.getAllViews();
+
+        System.out.println("Views: ");
+        views.forEach(System.out::println);
+
+        assertThat(views).isNotEmpty();
+        assertThat(views.stream().map(JenkinsView::getName)).contains("all");
+
+        jenkinsUtil.close();
+    }
+
+    @Test
+    public void checkGetAllJobs() throws IOException {
+        var jenkinsUtil = new JenkinsUtil();
+        var jobs = jenkinsUtil.getAllJobs();
+
+        System.out.println("Jobs: ");
+        jobs.forEach(System.out::println);
+
+        assertThat(jobs).isNotEmpty();
+        assertThat(jobs.stream().map(JenkinsJob::getName)).contains("Automation Center");
+
+        jenkinsUtil.close();
+    }
+}
