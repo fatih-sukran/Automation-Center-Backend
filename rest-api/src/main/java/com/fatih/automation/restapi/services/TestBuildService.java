275500354bb661e463e0f314c0132804dd392255
Fatih Şükran <fth.21.81@gmail.com>
2024-01-21 08:56:17 +0300

    * created the Service for TestBuild

---
+++ rest-api/src/main/java/com/fatih/automation/restapi/services/TestBuildService.java
@@ -0,0 +1,15 @@
+package com.fatih.automation.restapi.services;
+
+import com.fatih.automation.common.model.TestBuild;
+import com.fatih.automation.restapi.core.BaseCrud;
+import com.fatih.automation.restapi.repositories.TestBuildRepository;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class TestBuildService extends BaseCrud<TestBuild, Long, TestBuildRepository> {
+
+    public TestBuildService(TestBuildRepository repository) {
+        super(repository);
+    }
+}
