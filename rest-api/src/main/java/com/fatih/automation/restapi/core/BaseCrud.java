17077d0a000367418a311c756e63f00ea16bdd7c
Fatih Şükran <fth.21.81@gmail.com>
2024-01-21 08:53:34 +0300

    * makes BaserCrud abstract and concrete the methods

--- rest-api/src/main/java/com/fatih/automation/restapi/core/BaseCrud.java
+++ rest-api/src/main/java/com/fatih/automation/restapi/core/BaseCrud.java
@@ -1,13 +1,33 @@
 package com.fatih.automation.restapi.core;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
 import java.util.List;
 import java.util.Optional;
 
-public interface BaseCrud<T, ID> {
+@RequiredArgsConstructor
+public abstract class BaseCrud<T, ID, R extends JpaRepository<T, ID>> {
 
-    T save(T t);
-    Optional<T> findById(ID id);
-    List<T> findAll();
-    void delete(T t);
-    void deleteById(ID id);
+    protected final R repository;
+
+    public T save(T t) {
+        return repository.save(t);
+    }
+
+    public Optional<T> findById(ID id) {
+        return repository.findById(id);
+    }
+
+    public List<T> findAll() {
+        return repository.findAll();
+    }
+
+    public void delete(T t) {
+        repository.delete(t);
+    }
+
+    public void deleteById(ID id) {
+        repository.deleteById(id);
+    }
 }
