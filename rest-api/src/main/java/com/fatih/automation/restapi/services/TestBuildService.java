4b63d1082185f36e7053d0c73b793a57f413f925
Fatih Şükran <fth.21.81@gmail.com>
2024-01-22 21:19:22 +0300

    * added update status method

--- rest-api/src/main/java/com/fatih/automation/restapi/services/TestBuildService.java
+++ rest-api/src/main/java/com/fatih/automation/restapi/services/TestBuildService.java
@@ -1,6 +1,7 @@
 package com.fatih.automation.restapi.services;
 
 import com.fatih.automation.common.model.TestBuild;
+import com.fatih.automation.jenkins.utils.JenkinsUtil;
 import com.fatih.automation.restapi.core.BaseCrud;
 import com.fatih.automation.restapi.repositories.TestBuildRepository;
 import org.springframework.stereotype.Service;
@@ -9,7 +10,13 @@ import org.springframework.stereotype.Service;
 @Service
 public class TestBuildService extends BaseCrud<TestBuild, Long, TestBuildRepository> {
 
+
     public TestBuildService(TestBuildRepository repository) {
         super(repository);
+    }
+
+    public void updateStatus(TestBuild testBuild) {
+        var updatedTestBuild = JenkinsUtil.updateBuildStatus(testBuild);
+        repository.save(updatedTestBuild);
     }
 }
