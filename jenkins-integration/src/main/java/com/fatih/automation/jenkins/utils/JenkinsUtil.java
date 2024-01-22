f6a235d2dc69872f65e96da42eef62cf302b3d74
Fatih Şükran <fth.21.81@gmail.com>
2024-01-22 21:16:38 +0300

    * buildJob, updateBuildStatus methods added to JenkinsUtil

--- jenkins-integration/src/main/java/com/fatih/automation/jenkins/utils/JenkinsUtil.java
+++ jenkins-integration/src/main/java/com/fatih/automation/jenkins/utils/JenkinsUtil.java
@@ -1,16 +1,14 @@
 package com.fatih.automation.jenkins.utils;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fatih.automation.common.enums.BuildStatus;
 import com.fatih.automation.common.model.TestBuild;
+import com.fatih.automation.jenkins.mapper.BuildStatusMapper;
 import com.fatih.automation.jenkins.model.JenkinsJob;
 import com.fatih.automation.jenkins.model.JenkinsView;
 import com.offbytwo.jenkins.JenkinsServer;
 import com.offbytwo.jenkins.client.JenkinsHttpClient;
 import com.offbytwo.jenkins.model.BaseModel;
 import com.offbytwo.jenkins.model.Build;
-import com.offbytwo.jenkins.model.BuildResult;
-import com.offbytwo.jenkins.model.QueueReference;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.SneakyThrows;
@@ -66,8 +64,51 @@ public final class JenkinsUtil {
                 .toList();
     }
 
-    @SneakyThrows
-    public String buildJob(JenkinsJob jenkinsJob) {
-        return jenkinsServer.getJob(jenkinsJob.getName()).build(true).getQueueItemUrlPart();
+    public static String buildJob(String jobName) {
+        // TODO: Implement
+        throw new NotImplementedException();
     }
+
+    @SneakyThrows
+    public static Integer buildJob(JenkinsJob jenkinsJob, Map<String, String> parameters) {
+        var queueReference = jenkinsServer
+                .getJob(jenkinsJob.getName())
+                .build(parameters, true)
+                .getQueueItemUrlPart();
+        var parts = queueReference.split("\\D+");
+        return parts.length > 0 ? Integer.parseInt(parts[parts.length - 1]) : null;
+    }
+
+    @SneakyThrows //todo: return queue id
+    public static String buildJob(JenkinsJob jenkinsJob) {
+        return jenkinsServer
+                .getJob(jenkinsJob.getName())
+                .build(true)
+                .getQueueItemUrlPart();
+    }
+
+    @SneakyThrows
+    public static TestBuild updateBuildStatus(TestBuild testBuild) {
+        var url = "/job/Automation%20Center/api/json?tree=builds[number,result,queueId,url]";
+        var build = jenkinsHttpClient.get(url, ListBuild.class)
+                .getBuilds().stream()
+                .filter(b -> b.getQueueId() == testBuild.getQueueId())
+                .findFirst();
+        if (build.isPresent()) {
+            build.get().setClient(jenkinsHttpClient);
+            var buildResult = build.get().details().getResult();
+            var buildStatus = BuildStatusMapper.map(buildResult);
+            if (buildStatus != testBuild.getBuildStatus()) {
+                testBuild.setBuildStatus(buildStatus);
+            }
+        }
+        return testBuild;
+    }
+}
+
+@Data
+@EqualsAndHashCode(callSuper = true)
+class ListBuild extends BaseModel {
+    @JsonProperty("builds")
+    private List<Build> builds;
 }
