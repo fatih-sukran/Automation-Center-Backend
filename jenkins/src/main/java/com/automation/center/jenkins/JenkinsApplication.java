a7bd257a2d19306e71848b59f0050020d4da126d
Fatih Şükran <fth.21.81@gmail.com>
2024-07-28 22:57:20 +0300

    * feat: create jenkins manager bean

--- jenkins/src/main/java/com/automation/center/jenkins/JenkinsApplication.java
+++ jenkins/src/main/java/com/automation/center/jenkins/JenkinsApplication.java
@@ -1,17 +1,23 @@
 package com.automation.center.jenkins;
 
+import com.automation.center.jenkins.util.JenkinsManager;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Bean;
 
-@ComponentScan("com.automation.center.jenkins")
-@EntityScan("com.automation.center.jenkins")
 @SpringBootApplication
 public class JenkinsApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(JenkinsApplication.class, args);
+    }
+
+    @Bean
+    public JenkinsManager jenkinsManager(@Value("${jenkins.username}") String username,
+                                         @Value("${jenkins.password}") String password,
+                                         @Value("${jenkins.url}") String url) {
+        return new JenkinsManager(url, username, password);
     }
 
 }
