4b4f73c8baa73235428ba74202b9214bff11cc7c
Fatih Şükran <fth.21.81@gmail.com>
2024-07-28 23:27:54 +0300

    * feat: job xml for creating job in jenkins

--- jenkins/src/main/java/com/automation/center/jenkins/util/JenkinsManager.java
+++ jenkins/src/main/java/com/automation/center/jenkins/util/JenkinsManager.java
@@ -2,7 +2,10 @@ package com.automation.center.jenkins.util;
 
 import com.offbytwo.jenkins.JenkinsServer;
 import lombok.Getter;
+import lombok.SneakyThrows;
+import org.apache.commons.io.FileUtils;
 
+import java.io.File;
 import java.net.URI;
 
 @Getter
@@ -12,5 +15,12 @@ public class JenkinsManager {
     public JenkinsManager(String username, String password, String url) {
         this.server = new JenkinsServer(URI.create("http://localhost:8081"),
                 "admin", "admin");
+    }
+
+    @SneakyThrows
+    public String getJobXml(String description, String script) {
+        var file = new File(ClassLoader.getSystemResource("jenkins-xml/job.xml").toURI());
+        var content = FileUtils.readFileToString(file, "UTF-8");
+        return content.formatted(description, script);
     }
 }
