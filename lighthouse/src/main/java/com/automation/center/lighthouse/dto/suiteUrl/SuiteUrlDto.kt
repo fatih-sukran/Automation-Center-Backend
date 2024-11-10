3816a18fe2035ac42ad2a0c553f977ad3fabe74e
fatih.sukran <ycl.grr@gmail.com>
2024-11-10 12:44:39 +0300

    * feat: suite url mapper

--- lighthouse/src/main/java/com/automation/center/lighthouse/dto/suiteUrl/SuiteUrlDto.kt
+++ lighthouse/src/main/java/com/automation/center/lighthouse/dto/suiteUrl/SuiteUrlDto.kt
@@ -2,5 +2,6 @@ package com.automation.center.lighthouse.dto.suiteUrl
 
 data class SuiteUrlDto(
     var id: Long? = null,
+    var suiteId: Long? = null,
     var url: String? = null
 )
--- lighthouse/src/main/java/com/automation/center/lighthouse/mapper/SuiteItemMapper.java
+++ lighthouse/src/main/java/com/automation/center/lighthouse/mapper/SuiteItemMapper.java
@@ -5,8 +5,14 @@ import com.automation.center.lighthouse.dto.suiteUrl.AddSuiteUrlDto;
 import com.automation.center.lighthouse.dto.suiteUrl.SuiteUrlDto;
 import com.automation.center.lighthouse.model.SuiteUrl;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface SuiteItemMapper extends BaseMapper<SuiteUrl, SuiteUrlDto> {
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "testSuite.id", source = "suiteId")
     SuiteUrl toEntity(AddSuiteUrlDto addMetricUrDto);
+
+    @Mapping(target = "suiteId", source = "testSuite.id")
+    SuiteUrlDto toDto(SuiteUrl suiteUrl);
 }
